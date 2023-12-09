9c727c986fa4ecb58bc41fd2330a7e8eb5378ecf
PatelYash7 <yashpate0101@gmail.com>
2023-12-09 16:39:04 +0530

    first commit

---
+++ ClassLearning/Javascript/class1.js
@@ -0,0 +1,37 @@
+
+//Javascript is a interpreted Language
+console.log("Hello world ")
+
+// Basic javascript syntax
+
+// var num=0;
+
+// for (let i = 0; i <1000000000000; i++) {
+//     num=num+1;
+// }
+// console.log(num)
+
+//CallBack function.
+
+// function sum( num1, num2,fnTCall) {    
+//     fnTCall(num1+num2);
+// }
+// function displayResult(data){
+//     console.log("Sum of the Result is "+data)
+// }
+
+// sum(1,2,displayResult)
+
+// when we have to display the result of sum without callback then we haave two function to call.
+
+//Arrays
+
+const arr1=[1,2,3,4]
+const arr2=[5,6,7]
+
+arr2.forEach((i,n,arr)=>{
+    console.log(`${i} + ${n} + ${arr} `)
+})
+
+//Map Filter Find Sort  // will be covered after CallBacks
+
