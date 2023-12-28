3db7bbe701f4781ba521d0327fd19aa8dcec7a8d
PatelYash7 <yashpate0101@gmail.com>
2023-12-28 23:07:17 +0530

    TodoServer done

--- Assignment/week-2/02-nodejs/todoServer.js
+++ Assignment/week-2/02-nodejs/todoServer.js
@@ -41,9 +41,61 @@
  */
   const express = require('express');
   const bodyParser = require('body-parser');
-  
+  const fs= require('fs')
+  const port= 3000
   const app = express();
   
   app.use(bodyParser.json());
-  
+
+  let todos=[];
+
+  app.get('/todos',(req,res)=>{
+    res.json(todos);
+  })
+
+  app.get('/todos/:id',(req,res)=>{
+    const todo = todos.find(t=> t.id === parseInt(req.params.id));
+    if(!todo){
+      res.status(404).send('Not found');
+    }else{
+      res.json(todo);
+    }
+  })
+
+  app.post('/todos',(req,res)=>{
+    const newTodo={
+      id:Math.floor(Math.random()*1000000),
+      title: req.body.title,
+      isCompleted:req.body.isCompleted,
+      desc:req.body.desc
+    } 
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+  })
+
+  app.put('/todos/:id',(req,res)=>{
+    const todoIndex= todos.findIndex(t => t.id===parseInt(req.params.id));
+    if(todoIndex===-1){
+      res.status(404).send("Data Not available");
+    }else{
+      todos[todoIndex].desc=req.body.desc;
+      todos[todoIndex].title=req.body.title;
+      todos[todoIndex].isCompleted=req.body.isCompleted;
+      res.status(200).send();
+    }
+  })
+  app.delete('/todos/:id',(req,res)=>{
+    const todoIndex= todos.findIndex(t => t.id===parseInt(req.params.id));
+    if(todoIndex===-1){
+      res.status(404).send("No Data available")
+    }else{
+      todos.splice(todoIndex,1);
+      res.status(200).send();
+    }
+  })
+
+
+  app.listen(port,()=>{
+    console.log("Running on Port 3000")
+  })
   module.exports = app;
