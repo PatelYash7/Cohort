14d9dabd71984fbcdc3c52729874a4a78dfdfc3a
PatelYash7 <yashpate0101@gmail.com>
2024-01-25 01:31:42 +0530

    Removed File

--- Frontend/tailwind-Frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import RevenueCard from '../Component/RevenueCard'
-
-function App() {
-  return (
-      <RevenueCard title={"Amount Pending"} orderCount={24} amount={12156516.664} />
-  )
-}
-
-export default App
