070d1992ecdaebcd29135dd9ce8b8f1e4ab4a131
PatelYash7 <yashpate0101@gmail.com>
2024-01-29 11:41:11 +0530

    Added .env files

--- Assignment/week-3/03-mongo/db/index.js
+++ Assignment/week-3/03-mongo/db/index.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
-const {Schema,model}=mongoose;
+const {Schema,model,connect}=mongoose;
+require('dotenv').config();
+const url= process.env.MONGO_URI;
+console.log(url)
 
 // Connect to MongoDB
-mongoose.connect('zzzzz');
-
+const connectDB = async () =>{
+    try {
+        await connect(url);
+        console.log('MongoDB Connected...');
+    } catch (error) {
+        console.error(error.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+}
+connectDB();
 // Define schemas
 const AdminSchema = Schema({
 
--- Assignment/week-3/03-mongo/index.js
+++ Assignment/week-3/03-mongo/index.js
@@ -3,14 +3,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const adminRouter = require("./routes/admin")
 const userRouter = require("./routes/user");
+require('dotenv').config();
 
+console.log(process.env.MONGO_URI)
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
 const PORT = 3000;
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
