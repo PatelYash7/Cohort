ad07d1c89884eb4f27fb3816e7188c0223e9cb72
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-05-10 17:06:29 +0530

    Learned about layouts

--- NextJS/firstnextapp/src/app/layout.tsx
+++ NextJS/firstnextapp/src/app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      Hi There!
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ NextJS/firstnextapp/src/app/user/layout.tsx
@@ -0,0 +1,8 @@
+export default function ({children}:{children:React.ReactNode}){
+    return (
+        <div>
+            Navbar Header
+            {children}
+        </div>
+    )
+}
